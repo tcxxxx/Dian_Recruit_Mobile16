5f941dbd8e5a7d3d0da1746c9d848444900bdc9e
tcxxxx <luffyrocks@sina.com>
2016-11-05 02:35:01 +0800

    login page almost finished

--- js/login.js
+++ js/login.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
 	$("#login_btn").click(function(){
-		alert("hello");
 	})
+
+	/* bottom border style */
+	$("#login").click(function(){
+		$("#login_bottom").css("border-bottom","1px solid #FB9100");
+		$("#register_bottom").css("border-bottom","	none");
+
+		$("#loginarea").css("display","block");
+		$("#registerarea").css("display","none");
+	})
+
+	$("#register").click(function(){
+		$("#register_bottom").css("border-bottom","1px solid #FB9100");
+		$("#login_bottom").css("border-bottom","	none");
+	
+		$("#loginarea").css("display","none");
+		$("#registerarea").css("display","block");
+	})
+
 })
