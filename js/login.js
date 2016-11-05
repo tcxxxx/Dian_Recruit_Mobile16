ed62a8fb7492e097c21948824fd6c17bd9806779
tcxxxx <luffyrocks@sina.com>
2016-11-05 17:41:03 +0800

    side menu bar

--- js/login.js
+++ js/login.js
@@ -36,15 +36,24 @@ $(document).ready(function(){
 	/* 左侧导航栏 */
 	$("#menu").click(function(){
 		$(".left-navibar").show("slide",666);
+
+		$( '#' + $("#pagename").text() )
+			.css("border-right","3px solid #FB9100")
+			.css("padding-left","10%")
 	})
+
 	$("#left-navibar-bottom").click(function(){
 		$(".left-navibar").hide("slide",666);
+		$(".left-navibar li").css("border","none");
+		$(".left-navibar li").css("padding-left","0");	
 	})	
 
 	$(".left-navibar li").click(function(){
 		$(".left-navibar li").css("border","none");
 		$(".left-navibar li").css("padding-left","0");		
 		$(this).css("border-right","3px solid #FB9100");
-		$(this).css("padding-left","10%");		
+		$(this).css("padding-left","10%");
+
+		/*页面跳转*/		
 	})
 })
