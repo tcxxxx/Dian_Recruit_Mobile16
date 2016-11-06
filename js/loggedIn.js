52ec8e1bc36f59387f33109c56030cc7626287db
tcxxxx <luffyrocks@sina.com>
2016-11-06 16:42:03 +0800

    logged in page

---
+++ js/loggedIn.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	width = $("#box_line1").width();
+	$("#interval").css("height",0.1*width);
+
+	height = $(".process_box").width();
+	$(".process_box").css("height",height);
+})
