b0f5c99b19cc65da97ea6d98638a170cce3685e7
tcxxxx <luffyrocks@sina.com>
2016-11-06 19:24:30 +0800

    process

--- js/loggedIn.js
+++ js/loggedIn.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
+	window_width = $(window).width();
+
 	width = $("#box_line1").width();
 	$("#interval").css("height",0.1*width);
 
+	
 	height = $(".process_box").width();
-	$(".process_box").css("height",height);
-
+	if(window_width < 700)	
+		$(".process_box").css("height",height);
+	else
+		$(".process_box").css("height",0.6*height);		
 })
