794ab5713942087200ce26ba66fdfecf04e21e72
YuanXMjoy <850974686@qq.com>
2016-11-06 16:41:07 +0800

    complete the login and register function

--- js/login_action.js
+++ js/login_action.js
@@ -1,3 +1,218 @@
 /**
  * Created by JOYyuan on 16/11/6.
  */
+var wait = 60;
+var errorCode;
+var loginCode;
+$(document).ready(function(){
+    javascript:window.history.forward(1);
+    codeGet();
+    register();
+    login();
+    backspace();
+});
+function codeGet() {
+    $("#getCode").click(function () {
+        var codeBtn = document.getElementById("getCode");
+        var phoneNum = $("#input_userinfo").val();
+        if (phoneNum == "") {
+            $("#input_userinfo").addClass("error");
+        } else {
+            $.post("http://120.76.117.125:90/sendmsg/sendregmsg", {
+                "phone": phoneNum
+            }, function (res) {
+                var resultObj = JSON.parse(res);
+                var status = resultObj.status;
+                if (status) {
+                    time(codeBtn);
+                }
+            });
+
+        }
+    });
+
+}
+function time(o) {
+    if (wait == 0) {
+        o.removeAttribute("disabled");
+        o.innerHTML = "获取验证码";
+        wait = 60;
+    } else {
+        o.setAttribute("disabled", true);
+        o.innerHTML = "重发(" + wait + ")";
+        wait--;
+        setTimeout(function () {
+                time(o);
+            },
+            1000)
+    }
+}
+
+
+function isInteger(obj) {
+    var reg = /^1[3|4|5|7|8][0-9]{9}$/; //验证规则
+    var flag = reg.test(obj); //true
+    return flag;
+}
+function codeValidate(obj) {
+    var reg = /^\d{4}$/;
+    var flag = reg.test(obj);
+    return flag;
+}
+function regValidateError(account, code, pwd) {
+    var count = 0;
+    if (account == "") {
+        $("#input_userinfo").addClass("error");
+    } else {
+        var accBool = isInteger(account);
+        if (accBool) {
+            count++;
+        } else {
+            $("#input_userinfo").addClass("error");
+        }
+    }
+    if (code == "") {
+        $("#input_vericode").addClass("error");
+    } else {
+
+        var codeBool = codeValidate(code);
+        if (codeBool) {
+            count++;
+        } else {
+            $("#input_vericode").addClass("error");
+        }
+
+    }
+    if (pwd == "") {
+        $("#input_setpwd").addClass("error");
+    } else {
+        count++;
+    }
+
+    if (count == 3) {
+        errorCode = true;
+        $("#input_userinfo").removeClass("error");
+        $("#input_vericode").removeClass("error");
+        $("#input_setpwd").removeClass("error");
+    }
+}
+function checkInfo(num) {
+    $.post("http://120.76.117.125:90/user/getuserbyph", {
+        "phone":num
+    }, function (resUid) {
+        var resultObj=JSON.parse(resUid);
+        var uid=resultObj.uid;
+        if(uid==undefined){
+            sessionStorage.user=num;
+            window.location.href="loggedIn.html";
+        }else{
+            sessionStorage.user=num;
+            window.location.href="arrange.html";
+            sessionStorage.uid=uid;
+        }
+    })
+}
+function register() {
+    $("#registerBtn").click(function () {
+        var account = $("#input_userinfo").val();
+        var code = $("#input_vericode").val();
+        var pwd = $("#input_setpwd").val();
+        regValidateError(account, code, pwd);
+        if (errorCode) {
+            $.post("http://120.76.117.125:90/register/getreginfo", {
+                "phone": account,
+                "pwd": pwd,
+                "token": code
+            }, function (res) {
+                var resultObj = JSON.parse(res);
+                var status = resultObj.status;
+                errorCode = false;
+                switch (status) {
+                    case 0:
+                        alert("注册成功!");
+                        checkInfo(account);
+                        break;
+                    case 1:
+                        alert("验证码错误");
+                        break;
+                    case 2:
+                        alert("该手机号码已注册，密码已覆盖");
+                        checkInfo(account);
+                        break;
+                    case 3:
+                        alert("验证码已经过期");
+                        break;
+                }
+            });
+        }
+    });
+}
+
+function loginValidateError(user, pwd) {
+    var count = 0;
+    if (user == "") {
+        $("#userId").addClass("error");
+    } else {
+        var userBool = isInteger(user);
+        if (userBool) {
+            count++;
+        } else {
+            $("#userId").addClass("error");
+        }
+    }
+    if (pwd == "") {
+        $("#pwdLogin").addClass("error");
+    } else {
+        count++;
+    }
+    if (count == 2) {
+        loginCode = true;
+        $("#userId").removeClass("error");
+        $("#pwdLogin").removeClass("error");
+    }
+}
+function login() {
+    $("#login_btn").click(function () {
+        var user = $("#input_user").val();
+        var pwd = $("#input_pwd").val();
+        loginValidateError(user, pwd);
+        if (loginCode) {
+            $.post("http://120.76.117.125:90/login/getlogininfo", {
+                "login": user,
+                "pwd": pwd
+            }, function (res) {
+                loginCode = false;
+                var resultObj = JSON.parse(res);
+                var status = resultObj.status;
+                if (status == 0) {
+                    sessionStorage.user = user;
+                    checkInfo(user);
+                } else {
+                    alert("请检查输入是否正确！")
+                }
+            });
+        }
+    });
+}
+function backspace() {
+
+    if (event.keyCode == 8 && event.srcElement.tagName != "INPUT" && event.srcElement.type != "text")
+
+        event.returnValue = false;
+
+}
+
+if (navigator.appName == "Microsoft Internet Explorer") {
+
+    window.history.forward(1);
+
+}
+
+else // if it is Mozilla than
+
+{
+
+    window.history.forward(-1);
+
+}
+
